b9a89e31cbdc7edc747228e0e66b163090f108ce
pipo <pipos_rgt@hotmail.com>
2017-08-16 21:17:28 +0200

    listNewFinish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { HttpModule } from '@angular/http';
 
+
 import { MyApp } from './app.component';
 import { InicioPage } from '../pages/inicio/inicio';
 import { PerfilPage } from '../pages/perfil/perfil';
