b9f4f5853ac74ac0a4654378e7aa13de848d046e
thomasklz <pipos_rgt@hotmail.com>
2019-01-15 14:46:25 -0500

    filter book

--- src/pages/libros/libros.ts
+++ src/pages/libros/libros.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { BilibotecaServiceProvider } from '../../providers/biliboteca-service/biliboteca-service';
+import { FormControl } from '@angular/forms';
+import 'rxjs/add/operator/debounceTime';
 
 @IonicPage()
 @Component({
@@ -12,17 +14,29 @@ export class LibrosPage {
   public txtbook: string;
   items: string[];
   public book: any;
+  searchTerm: string = '';
+  searchControl: FormControl;
+  searching: any = false;
   constructor(public navCtrl: NavController, public navParams: NavParams, public searchbookServe: BilibotecaServiceProvider) {
     this.book = [];
+    this.searchControl = new FormControl();
   }
 
+  ionViewDidLoad() {
+    this.SearchBooks();
+    this.searchControl.valueChanges.debounceTime(700).subscribe(search => {
+      this.searching = false;
+      this.SearchBooks();
+    });
+  }
    SearchBooks() {
     this.searchbookServe.searchBook(this.txtbook).subscribe(books => {
-    console.log(books.Biblios);
     this.book = books['Biblios'];
     });
   }
-
+   onSearchInput() {
+     this.searching = true;
+   }
   goToDetailBook(detailBook) {
     this.navCtrl.push('DetalleLibrosPage', {
       'id': detailBook
